a6259437e5901e9753069e99ba876bfaf683e845
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2024-08-14 01:18:33 +0200

    Create genvfy1a.py

---
+++ fysik/1a/genvfy1a.py
@@ -0,0 +1,9 @@
+m = "**v%d %s (%d min):**  "
+d = ["ti", "on", "fr"]
+t = [85, 95, 80]
+for i in range(33, 53):
+    for j in range(0, 3):
+        print(m % (i, d[j], t[j]))
+for i in range(1, 25):
+    for j in range(0, 3):
+        print(m % (i, d[j], t[j]))
